95f03fa502f986608c3f0118682774abf7b9d55f
ASHRIBAD PANDA <133385084+ashribad15@users.noreply.github.com>
2024-06-28 15:28:27 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,98 @@
+abstract class Employee
+{
+   private String name;
+   private int id;
+
+   public Employee(String name, int id)
+   {
+    this.name=name;
+    this.id=id;
+   }
+   public String getName()
+   {
+        return name;
+   }
+   public int getId()
+   {
+    return id;
+   }
+     
+      public abstract double calculateSalary();
+      
+      @Override 
+      public String toString()
+      {
+        return "Employee[name="+name+",id="+id+", salary="+calculateSalary()+" ]";
+      }
+
+}
+
+class FullTimeEmployee extends Employee
+{
+    private double monthlySalary;
+
+    public FullTimeEmployee(String name, int id, double monthlySalary)
+    {
+        super(name, id);
+        this.monthlySalary=monthlySalary;
+
+    }
+
+
+    @Override
+
+    public double calculateSalary()
+    {
+        return monthlySalary;
+
+    }
+}
+
+
+class PartTimeEmployee extends Employee
+{
+       private int hoursWorked;
+       private double hourlyRate;
+
+       public PartTimeEmployee( String name, int id, int hoursWorked, double hourlyRate)
+       {
+           super(name, id);
+           this.hoursWorked=hoursWorked;
+           this.hourlyRate=hourlyRate;
+
+       }
+
+
+       @Override
+       public double calculateSalary()
+       {
+        return (hoursWorked*hourlyRate);
+       }
+}
+
+class PayRollSystem
+{
+    private ArrayList<Employee> employeeList;
+
+    public PayRollSystem()
+    {
+        employeeList = new ArrayList<>();
+    }
+
+
+    public void addEmployee(Employee employee )
+    {
+         employeeList.add(employee);
+    }
+    
+
+}
+ 
+public class Main
+{
+    public static void main()
+    {
+        System.out.println("Hello World!");
+    }
+
+}
